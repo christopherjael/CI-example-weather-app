877c427e71b2a5998c9de5f158e0c58db905f84d
Christopher Montero D'oleo <cristhofermontero1@gmail.com>
2023-02-03 19:56:23 +0000

    modify: adding APIKEY and modify server.js for accept .env file

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
-
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 5004;
 
 //registering middlewares
-dotenv.config();
 app.use(express.json());
 app.use(cors());
 
